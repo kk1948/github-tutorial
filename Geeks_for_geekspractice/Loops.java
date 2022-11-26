4fe41039f3c5f47852a27e6cc03a25a4f9a5e7c3
kk1948 <rai123vishwa@gmail.com>
2022-11-26 19:37:13 +0530

    Second Commit

---
+++ Geeks_for_geekspractice/Loops.java
@@ -0,0 +1,36 @@
+package Geeks_for_geekspractice;
+
+public class Loops {
+    public static void main(String[] args) {
+
+
+        // simple for loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Kishan");
+
+        }
+
+        // above stmt can also be defined like this
+        int i;
+        for ( i = 0; i < 10; i++) {
+            System.out.println("Kishan");
+
+        }
+        System.out.println(i);
+        // infinite loop example
+//        for ( ;  ; ){
+//            System.out.println("kishan");
+//            System.out.println("nikhil");
+//        }
+
+        // another example here condition is true
+//        for ( ; true; ){
+//            System.out.println("kishan");
+//        }
+
+    }
+
+
+
+
+}

bb134ac6f137b773aedf9f9794f425091feaee7d
kk1948 <rai123vishwa@gmail.com>
2022-12-15 17:33:16 +0530

    New commit added

--- Oops_with_Kunal/Oopskunal.java
+++ Oops_with_Kunal/Oopskunal.java
@@ -1,17 +1,25 @@
-package Geeks_for_geekspractice;
+package Oops_with_Kunal;
 
 public class Oopskunal {
     public static void main(String[] args) {
 
         Student kishan = new Student();
+
+        kishan.name = "kishan";
+        kishan.marks = 95;
+        kishan.age = 24;
+
+
         System.out.println(kishan);
         System.out.println(kishan.name);
+        System.out.println(kishan.age);
+        System.out.println(kishan.marks);
     }
 }
 //crates a class
 // for every student1
  class Student {
     String name;
-    int roll_No;
-    int marks;
+    int age;
+    float marks;
  }

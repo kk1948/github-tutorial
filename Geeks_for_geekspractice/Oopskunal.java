048e3f4dec4cf17230a75b98c79f4ca6d4627975
kk1948 <rai123vishwa@gmail.com>
2022-12-15 16:48:54 +0530

    New file added

---
+++ Geeks_for_geekspractice/Oopskunal.java
@@ -0,0 +1,17 @@
+package Geeks_for_geekspractice;
+
+public class Oopskunal {
+    public static void main(String[] args) {
+
+        Student kishan = new Student();
+        System.out.println(kishan);
+        System.out.println(kishan.name);
+    }
+}
+//crates a class
+// for every student1
+ class Student {
+    String name;
+    int roll_No;
+    int marks;
+ }
